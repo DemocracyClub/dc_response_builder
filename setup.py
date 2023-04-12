1d6104aea95965dbe76b881a36975611c535b4dc
Sym Roe <sym.roe@talusdesign.co.uk>
2023-04-12 16:50:52 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="dc_response_builder",
-    version="0.0.9",
+    version="0.0.10",
     description="Builds API responses",
     author="Sym Roe",
     author_email="sym.roe@democracyclub.org.uk",
