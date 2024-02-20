104aee06ed3e8499b79c9cf5a866f7bce2a59e3b
Sym Roe <sym.roe@talusdesign.co.uk>
2024-02-20 17:09:40 +0000

    Add electoral services to response

--- response_builder/v1/generated_responses/root_responses.py
+++ response_builder/v1/generated_responses/root_responses.py
@@ -1,11 +1,24 @@
 from response_builder.v1.builders.ballots import StockLocalBallotBuilder
 from response_builder.v1.builders.base import RootBuilder
 from response_builder.v1.builders.polling_stations import PollingStationBuilder
-from response_builder.v1.models.polling_stations import Station, StationProperties
+from response_builder.v1.models.councils import ElectoralServices
+from response_builder.v1.models.polling_stations import (
+    Station,
+    StationProperties,
+)
 
 SINGLE_LOCAL_FUTURE_BALLOT = RootBuilder().with_ballot(
     StockLocalBallotBuilder().build()
-)
+).with_electoral_services(
+    ElectoralServices(
+        council_id="STO",
+        name="Stroud Council",
+        address="123 Stroud Street",
+        email="electoralservices@stroud.gov.uk",
+        nation="England",
+        phone="123456",
+        postcode="GL51AA",
+        website="https://stroud.gov.uk"))
 
 POLLING_STATION = (
     PollingStationBuilder().set("polling_station_known", True).build()
