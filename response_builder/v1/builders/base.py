e6ca61cb85447b07f39887958565d3d41758c7e7
Virginia Dooley <vcdooley@gmail.com>
2024-03-04 12:01:03 +0000

    Add candidates list to ballot

--- response_builder/v1/builders/base.py
+++ response_builder/v1/builders/base.py
@@ -11,6 +11,14 @@ from response_builder.v1.models.base import (
     RootModel,
     VotingSystem,
     Address,
+    Candidate,
+    Party,
+    Person,
+    Candidate,
+    Leaflet,
+    PreviousParty,
+    EmailStr,
+    PreviousParty,
 )
 from response_builder.v1.models.councils import ElectoralServices
 from response_builder.v1.models.polling_stations import PollingStation
@@ -115,6 +123,99 @@ class RootBuilder(AbstractBuilder[RootModel]):
     
     def without_ballot(self):
         self._values.pop("dates", None)
+        return self
+
+    def with_candidates(self, candidates = list[Candidate]):
+        if not self._values["dates"]:
+            raise ValueError("Can't add candidates with no dates")
+
+        candidates = [
+            Candidate(
+                list_position=3,
+                party=Party(
+                    party_name="Labour Party",
+                    party_id="PP53",
+                ),
+                person=Person(
+                    name="Mr. Foo Bar",
+                    ynr_id=123,
+                    absolute_url="https://candidates.democracyclub.org.uk/person/123",
+                    email=EmailStr("person@website.com"),
+                    photo_url="https://s3.eu-west-2.amazonaws.com/static-candidates.democracyclub.org.uk/media/cache/fc/123/fc123.jpg",
+                    leaflets=[
+                        Leaflet(
+                            leaflet_id=123,
+                            thumb_url="https://electionleaflets.org/leaflets/123/",
+                            leaflet_url="https://electionleaflets.org/leaflets/123/",
+                        )
+                    ],
+                ),
+                previous_party_affiliations=PreviousParty(
+                    name="Labour Party",
+                    abbreviation="Lab",
+                    colour="blue",
+                    url="https://labour.com",
+                ),
+            ),
+            Candidate(
+                list_position=1,
+                party=Party(
+                    party_name="Conservative Party",
+                    party_id="PP52",
+                ),
+                person=Person(
+                    name="Mrs. Bar Foo",
+                    ynr_id=456,
+                    absolute_url="https://candidates.democracyclub.org.uk/person/456",
+                    email=EmailStr("person2@website.com"),
+                    photo_url="https://s3.eu-west-2.amazonaws.com/static-candidates.democracyclub.org.uk/media/cache/fc/456/fc456.jpg",
+                    leaflets=[
+                        Leaflet(
+                            leaflet_id=456,
+                            thumb_url="https://electionleaflets.org/leaflets/456/",
+                            leaflet_url="https://electionleaflets.org/leaflets/456/",
+                        )
+                    ],
+                ),
+                previous_party_affiliations=PreviousParty(
+                    name="Conservative Party",
+                    abbreviation="Con",
+                    colour="red",
+                    url="https://conservatives.com",
+                ),
+            ),
+            Candidate(
+                list_position=2,
+                party=Party(
+                    party_name="Liberal Democrats Party",
+                    party_id="PP90",
+                ),
+                person=Person(
+                    name="Dr. Baz Qux",
+                    ynr_id=123,
+                    absolute_url="https://candidates.democracyclub.org.uk/person/789",
+                    email=EmailStr("dr_person@website.com"),
+                    photo_url="https://s3.eu-west-2.amazonaws.com/static-candidates.democracyclub.org.uk/media/cache/fc/789/fc789.jpg",
+                    leaflets=[
+                        Leaflet(
+                            leaflet_id=789,
+                            thumb_url="https://electionleaflets.org/leaflets/789/",
+                            leaflet_url="https://electionleaflets.org/leaflets/789/",
+                        )
+                    ],
+                ),
+                previous_party_affiliations=PreviousParty(
+                    name="Liberal Democrats Party",
+                    abbreviation="Lib Dem",
+                    colour="yellow",
+                    url="https://libdem.com",
+                ),
+            ),
+        ]
+
+        date = self._values["dates"][0]
+        for ballot in date.ballots:
+            ballot.candidates = candidates
         return self
 
     def with_electoral_services(self, electoral_services: ElectoralServices):
